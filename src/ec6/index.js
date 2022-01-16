b321db77e121627ac2aabdabeef162811a1bcc2c
Dp100131 <apintod@uninorte.edu.co>
2022-01-16 17:45:57 -0500

    clase 2

--- src/ec6/index.js
+++ src/ec6/index.js
@@ -9,6 +9,7 @@ function newFunction(name1, age1, country1){
 }
 
 // es6
+console.group("Clase 1");
 
 function newFunction2(name = 'Daniel', age = '20', country = 'COL'){
 
@@ -23,5 +24,91 @@ let hello = 'Hello';
 let world = 'world';
 
 let epicPhrase = `${hello} ${world}`;
-
 console.log(epicPhrase);
+
+console.groupEnd();
+
+console.group('Clase 2');
+
+let lorem = "frase epica uno \n" 
++ "otra frase epica";
+console.log(lorem);
+
+console.log('');
+
+// es6
+let lorem2 = `Otra frase epica
+que necesitamos`;
+
+console.log(lorem2);
+
+console.log('');
+console.log('');
+
+let person = {
+
+    name: 'Daniel',
+    age: 20,
+    country: 'Colombia'
+
+}
+
+console.log(person.name, person.age, person.country);
+
+console.log('');
+
+//es6
+
+let {name, age, country} = person;
+
+console.log(name);
+
+console.log('');
+
+console.log('');
+
+let team1 = ['Henry', 'Sergio', 'Sharon']
+let team2 = ['Natalia', 'Isabella', 'Juancho']
+
+let education = ['David', ...team1, ...team2]
+
+console.log(education);
+
+console.log('');
+
+console.log('');
+
+{
+
+    var globalvar = 'Global Var';
+
+}
+
+{
+
+    let globalLet = 'Global let';
+
+}
+console.log(globalvar);
+/* console.log(globalLet); */
+
+console.log('');
+
+console.log('');
+
+const A = 'b';
+
+A = 'a';
+
+console.log(A);
+
+console.log('');
+
+
+
+
+console.groupEnd();
+
+console.group('Clase 3');
+
+console.groupEnd();
