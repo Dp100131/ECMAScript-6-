19a1987624289e75f63a20f0c599ecc871270fc4
Dp100131 <apintod@uninorte.edu.co>
2022-01-17 11:06:38 -0500

    Clase 9 entre comillas jajajaj xd

--- src/ec6/index.js
+++ src/ec6/index.js
@@ -320,6 +320,57 @@ console.groupEnd();
 
 console.group('Clase 7');
 
+// es9 
+
+const obj50 = {
+
+    name50: 'Daniel',
+    age50: 32,
+    country50: 'COL',
+
+};
+
+let {...all} = obj50;
+
+console.log(all);
+
+const obj51 = {
+
+    ...obj50,
+    lindo: false,
+
+};
+
+console.log(obj51);
+
+const holamundo2 = () => {
+
+    return new Promise((resolve, reject) => {
+
+        (true)
+            ? setTimeout(() => resolve('Hello world'), 3000)
+            : reject(new Error('Test Error'))
+
+    });
+
+};
+
+holamundo2()
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finalizo'))
+
+
+const regexData = /([0 - 9]{4})-([0 - 9]{2})-([0 - 9]{2})/;
+const match = regexData.exec('2018-04-20');
+const year = match[1]
+const month = match[2]
+const day = match[3]
+
+console.log(day, month, year);
+
+
+
 console.groupEnd();
 
 console.group('Clase 8');
